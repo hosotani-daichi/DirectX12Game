f047e452cc274fe05034445e25948d64f770458a
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2024-06-19 16:12:45 +0900

    3_00

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,15 +1,12 @@
+#include "object3d.hlsli"
+
 struct Material
 {
     float32_t4 color;
 };
 ConstantBuffer<Material> gMaterial : register(b0);
 
-struct PixelShaderOutput
-{
-    float32_t4 color : SV_TARGET0;
-};
-
-PixelShaderOutput main()
+PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
     output.color = gMaterial.color;
