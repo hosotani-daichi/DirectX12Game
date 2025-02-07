055ae1769c6b1363598652cf9a9491d81b79abdc
細谷　大智 <146915322+hosotani-daichi@users.noreply.github.com>
2025-02-07 10:58:50 +0900

    PG3_01_04

--- main.cpp
+++ main.cpp
@@ -421,80 +421,6 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 	return modelData;
 }
 
-// ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename) {
-//	// 1. 中で必要となる変数の宣言
-//	ModelData modelData; // 構築するModelData
-//	std::vector<Vector4> positions; // 位置
-//	std::vector<Vector3> normals; // 法線
-//	std::vector<Vector2> texcoords; // テクスチャ座標
-//	std::string line; // ファイルから読んだ1行を格納するもの
-//
-//	// 2. ファイルを開く
-//	std::ifstream file(directoryPath + "/" + filename); // ファイルを開く
-//	assert(file.is_open()); // とりあえず開けなかったら止める
-//	// 3. 実際にファイルを読み、ModelDataを構築していく
-//	while (std::getline(file, line))
-//	{
-//		std::string identifier;
-//		std::istringstream s(line);
-//		s >> identifier; // 先頭の識別子を読む
-//
-//		// identifierに応じた処理
-//		if (identifier == "v") {
-//			Vector4 position;
-//			s >> position.x >> position.y >> position.z;
-//			position.w = 1.0f;
-//			positions.push_back(position);
-//		} else if (identifier == "vt") {
-//			Vector2 texcoord;
-//			s >> texcoord.x >> texcoord.y;
-//			texcoords.push_back(texcoord);
-//		} else if (identifier == "vn") {
-//			Vector3 normal;
-//			s >> normal.x >> normal.y >> normal.z;
-//			normals.push_back(normal);
-//		} else if (identifier == "f") {
-//			VertexData triangle[3];
-//
-//			// 面は三角形限定。その他は未対応
-//			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
-//				std::string vertexDefinition;
-//				s >> vertexDefinition;
-//				// 頂点の要素へのIndexは「位置/UV/法線」で格納されているので、分解してIndexを取得する
-//				std::istringstream v(vertexDefinition);
-//				uint32_t elementIndices[3];
-//				for (int32_t element = 0; element < 3; ++element) {
-//					std::string index;
-//					std::getline(v, index, '/'); // /区切りでインデックスを読んでいく
-//					elementIndices[element] = std::stoi(index);
-//				}
-//				// 要素へのIndexから、実際の要素を値を取得して、頂点を構築する
-//				Vector4 position = positions[elementIndices[0] - 1];
-//				Vector2 texcoord = texcoords[elementIndices[1] - 1];
-//				Vector3 normal = normals[elementIndices[2] - 1];
-//				//VertexData vertex = { position, texcoord, normal };
-//				//modelData.vertices.push_back(vertex);
-//				position.x *= -1.0f;
-//				texcoord.y = 1.0f - texcoord.y;
-//				normal.x *= -1.0f;
-//				triangle[faceVertex] = { position, texcoord, normal };
-//			}
-//			// 頂点を逆順で登録することで、周り順を逆にする
-//			modelData.vertices.push_back(triangle[2]);
-//			modelData.vertices.push_back(triangle[1]);
-//			modelData.vertices.push_back(triangle[0]);
-//		} else if (identifier == "mtllib") {
-//			// materialTemplateLibraryファイルの名前を取得する
-//			std::string materialFilename;
-//			s >> materialFilename;
-//			// 基本的にobjファイルと同一階層にmtlは存在させるので、ディレクトリ名とファイル名を渡す
-//			modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
-//		}
-//	}
-//	// 4. ModelDataを返す
-//	return modelData;
-// }
-
 D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(ID3D12DescriptorHeap* descriptorHeap, uint32_t descriptorSize, uint32_t index) {
 	D3D12_CPU_DESCRIPTOR_HANDLE handleCPU = descriptorHeap->GetCPUDescriptorHandleForHeapStart();
 	handleCPU.ptr += (descriptorSize * index);
@@ -544,7 +470,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ウインドウプロシージャ
 	wc.lpfnWndProc = WindowProc;
 	// ウインドウクラス名
-	wc.lpszClassName = L"CG2WindowClass";
+	wc.lpszClassName = L"CG3WindowClass";
 	// インスタンスハンドル
 	wc.hInstance = GetModuleHandle(nullptr);
 	// カーソル
@@ -575,7 +501,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ウインドウの生成
 	HWND hwnd = CreateWindow(
 	    wc.lpszClassName,     // 利用するクラス名
-	    L"CG2",               // タイトルバーの文字
+	    L"CG3",               // タイトルバーの文字
 	    WS_OVERLAPPEDWINDOW,  // よく見るウインドウスタイル
 	    CW_USEDEFAULT,        // 表示X座標(Windowsに任せる)
 	    CW_USEDEFAULT,        // 表示Y座標(WindowsOSに任せる)
@@ -980,26 +906,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
 	std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size()); // 頂点データをリソースのコピー
 
-	// 左下
-	// vertexData[0].position = { -0.5f,-0.5f,0.0f,1.0f };
-	// vertexData[0].texcoord = { 0.0f,1.0f };
-	////上
-	// vertexData[1].position = { 0.0f,0.5f,0.0f,1.0f };
-	// vertexData[1].texcoord = { 0.5f,0.0f };
-	////右下
-	// vertexData[2].position = { 0.5f,-0.5f,0.0f,1.0f };
-	// vertexData[2].texcoord = { 1.0f,1.0f };
-
-	////左下2
-	// vertexData[3].position = { -0.5f,-0.5f,0.5f,1.0f };
-	// vertexData[3].texcoord = { 0.0f,1.0f };
-	////上2
-	// vertexData[4].position = { 0.0f,0.0f,0.0f,1.0f };
-	// vertexData[4].texcoord = { 0.5f,0.0f };
-	////右下2
-	// vertexData[5].position = { 0.5f,-0.5f,-0.5f,1.0f };
-	// vertexData[5].texcoord = { 1.0f,1.0f };
-
 	// 頂点バッファビューを作成する
 	D3D12_VERTEX_BUFFER_VIEW vertexBufferViewSprite{};
 	// リソースの先頭のアドレスから使う
@@ -1120,15 +1026,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	transformationMatrixResourceSprite->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixDataSprite));
 	// 単位行列を書き込んでおく
 	*transformationMatrixDataSprite = MakeIdentity4x4();
-
-	////TransformationMatrix用のリソースを作る。Matrix4x41つ分のサイズを用意
-	// ID3D12Resource* transformationMatrixResource = CreateBufferResource(device, sizeof(Matrix4x4));
-	////データを書き込む
-	// Matrix4x4* transformationMatrixData = nullptr;
-	////書き込むためのアドレスを取得
-	// transformationMatrixResource->Map(0, nullptr, reinterpret_cast<void**>(&transformationMatrixDataSprite));
-	////単位行列を書き込んでおく
-	//*transformationMatrixData = MakeIdentity4x4();
 
 	// Textureを読んで転送する
 	DirectX::ScratchImage mipImages = LoadTexture("Resources/circle.png");
@@ -1342,11 +1239,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 			commandList->IASetVertexBuffers(0, 1, &vertexBufferViewSprite); // VBVを設定
 			commandList->IASetIndexBuffer(&indexBufferViewSprite);
-
-			// TransformationMatrixCBufferの場所を設定
-			/*commandList->SetGraphicsRootConstantBufferView(1, transformationMatrixResourceSprite->GetGPUVirtualAddress());*/
-			// 描画
-			// commandList->DrawIndexedInstanced(6, 1, 0, 0,0);
 
 			// 実際のcomandListのImGuiの描画コマンドを挟む
 			ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), commandList); //////////
